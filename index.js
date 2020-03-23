111adfe345906a7124abf8195e5ad12f89fce38e
Sergo <venyae2@gmail.com>
2020-03-23 16:46:13 +0200

    fix(LV-1385): explicitly load crypto

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+const crypto = require('crypto');
 var r;
+
+if (typeof crypto.randomBytes !== 'function') {
+  throw new Error('randomBytes method is not supported by loaded crypto implementation');
+}
 
 module.exports = function rand(len) {
   if (!r)
@@ -16,50 +21,6 @@ Rand.prototype.generate = function generate(len) {
   return this._rand(len);
 };
 
-// Emulate crypto API using randy
 Rand.prototype._rand = function _rand(n) {
-  if (this.rand.getBytes)
-    return this.rand.getBytes(n);
-
-  var res = new Uint8Array(n);
-  for (var i = 0; i < res.length; i++)
-    res[i] = this.rand.getByte();
-  return res;
+  return crypto.randomBytes(n);
 };
-
-if (typeof self === 'object') {
-  if (self.crypto && self.crypto.getRandomValues) {
-    // Modern browsers
-    Rand.prototype._rand = function _rand(n) {
-      var arr = new Uint8Array(n);
-      self.crypto.getRandomValues(arr);
-      return arr;
-    };
-  } else if (self.msCrypto && self.msCrypto.getRandomValues) {
-    // IE
-    Rand.prototype._rand = function _rand(n) {
-      var arr = new Uint8Array(n);
-      self.msCrypto.getRandomValues(arr);
-      return arr;
-    };
-
-  // Safari's WebWorkers do not have `crypto`
-  } else if (typeof window === 'object') {
-    // Old junk
-    Rand.prototype._rand = function() {
-      throw new Error('Not implemented yet');
-    };
-  }
-} else {
-  // Node.js or Web worker with no crypto support
-  try {
-    var crypto = require('crypto');
-    if (typeof crypto.randomBytes !== 'function')
-      throw new Error('Not supported');
-
-    Rand.prototype._rand = function _rand(n) {
-      return crypto.randomBytes(n);
-    };
-  } catch (e) {
-  }
-}
